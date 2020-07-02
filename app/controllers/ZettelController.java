22363c0c21e195fb9ca298e9b8ef8a75d83f520b
aquast <quast@hbz-nrw.de>
2020-07-02 09:35:22 +0200

    Revert "Frl 302"

--- app/controllers/ZettelController.java
+++ app/controllers/ZettelController.java
@@ -303,21 +303,20 @@ public class ZettelController extends Controller {
 		final String[] callback =
 				request() == null || request().queryString() == null ? null
 						: request().queryString().get("callback");
-		String orcidUrl = "https://pub.orcid.org/v3.0/expanded-search";
+		String orcidUrl = "https://pub.orcid.org/v2.1/search";
 		WSRequest request = ws.url(orcidUrl);
 		WSRequest complexRequest = request.setHeader("accept", "application/json")
-				.setRequestTimeout(5000).setQueryParameter("q", q);
+				.setRequestTimeout(5000).setQueryParameter("q",
+						"family-name:" + q + " OR given-names:" + q + " OR orcid:" + q);
 		return complexRequest.setFollowRedirects(true).get().thenApply(response -> {
-			JsonNode hits = response.asJson().at("/expanded-result");
+			JsonNode hits = response.asJson().at("/result");
 			List<Map<String, String>> result = new ArrayList<>();
 			hits.forEach((hit) -> {
 
-				String id = hit.at("/orcid-id").asText();
-				String lastName = hit.at("/family-names").asText();
-				String firstName = hit.at("/given-names").asText();
+				String id = hit.at("/orcid-identifier/uri").asText();
 				Map<String, String> m = new HashMap<>();
-				m.put("label", lastName + ", " + firstName + " | " + id);
-				m.put("value", "https://orcid.org/" + id);
+				m.put("label", id);
+				m.put("value", id);
 				result.add(m);
 			});
 			String searchResult = ZettelHelper.objectToString(result);
