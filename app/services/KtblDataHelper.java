255c8d65f58e8b5b925243206dc9098139509e07
Andres Quast <quast@hbz-nrw.de>
2020-10-09 15:30:44 +0200

    grinding down the properties problem #3

--- app/services/KtblDataHelper.java
+++ app/services/KtblDataHelper.java
@@ -30,25 +30,25 @@ public class KtblDataHelper {
 			livestock.put("info.ktbl.livestock.turkey.de", "Pute");
 			livestock.put("info.ktbl.livestock.duck.de", "Ente");
 		
-		
+		livestock.putAll(livestockProp);
 		
 		return livestock;
 	}
 
 	public static Properties loadPropertiesFile(String fName, Properties properties) {
-		try (InputStream propStream = Thread.currentThread().getContextClassLoader()
+		/*try (InputStream propStream = Thread.currentThread().getContextClassLoader()
 				.getResourceAsStream("conf/" + fName)) {
 			properties.load(propStream);
 		} catch (IOException e) {
-			System.out.println(e.getStackTrace());
+			e.printStackTrace(); */
 			Properties defaultProperties = new Properties();
 			defaultProperties.put(ZettelModel.ZETTEL_NULL, "Bitte wählen Sie...");
 			defaultProperties.put("info.ktbl.livestock.cattle.de", "Rind");
 			defaultProperties.put("info.ktbl.livestock.pork.de", "Schwein");
 			
 			return defaultProperties;
-		}
-		return properties;
+		//}
+		//return properties;
 	}
 
 
