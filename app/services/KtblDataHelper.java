6e63d0096c0410912e16f841ab1a9b1d8acaeac8
Andres Quast <quast@hbz-nrw.de>
2020-10-09 18:58:04 +0200

    grinding down the properties problem #5

--- app/services/KtblDataHelper.java
+++ app/services/KtblDataHelper.java
@@ -5,6 +5,7 @@ package services;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -21,33 +22,29 @@ public class KtblDataHelper {
 	public static LinkedHashMap<String,String> getLivestockType() {
 		LinkedHashMap<String,String> livestock = new LinkedHashMap<String,String>();
 		
-		Map<String, String> livestockProp = (Map) loadPropertiesFile("ktbl.livestock.properties", new Properties());
+		Properties livestockProp = loadPropertiesFile("ktbl.livestock.properties", new Properties());
 		
-			livestock.put(ZettelModel.ZETTEL_NULL, "Bitte wählen Sie...");
-			livestock.put("info.ktbl.livestock.cattle.de", "Rind");
-			//livestock.put("info.ktbl.livestock.pork.de", "Schwein");
-			livestock.put("info.ktbl.livestock.chicken.de", "Huhn");
-			livestock.put("info.ktbl.livestock.turkey.de", "Pute");
-			livestock.put("info.ktbl.livestock.duck.de", "Ente");
+		livestock.put(ZettelModel.ZETTEL_NULL, "Bitte wählen Sie...");
 		
-		livestockProp.putAll(livestock);
+		Enumeration<Object> livestockEnum = livestockProp.keys();
+		while(livestockEnum.hasMoreElements()) {
+			String key = (String) livestockEnum.nextElement();
+			livestock.put(key, livestockProp.getProperty(key));
+		}
 		
 		return livestock;
 	}
 
 	public static Properties loadPropertiesFile(String fName, Properties properties) {
-		try (InputStream propStream = Thread.currentThread().getContextClassLoader()
-				.getResourceAsStream(fName)) {
-			properties.load(propStream);
-		} catch (IOException e) {
-			e.printStackTrace();
-			Properties defaultProperties = new Properties();
-			defaultProperties.put(ZettelModel.ZETTEL_NULL, "Bitte wählen Sie...");
-			defaultProperties.put("info.ktbl.livestock.cattle.de", "Rind");
-			defaultProperties.put("info.ktbl.livestock.pork.de", "Schwein");
-			defaultProperties.put("info.ktbl.livestock.pork.de", "Schaf");
-			
-			return defaultProperties;
+
+		properties = new Properties();
+		InputStream propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fName);
+		if(propStream !=null) {
+			try{		
+				properties.load(propStream);
+			} catch (IOException e) {
+				System.out.println(e.toString());
+			}
 		}
 		return properties;
 	}
