18b7fa18f154174a4e83fa53e64c7df7829528a7
Andres Quast <quast@hbz-nrw.de>
2020-10-09 15:51:12 +0200

    grinding down the properties problem #4

--- app/services/KtblDataHelper.java
+++ app/services/KtblDataHelper.java
@@ -21,34 +21,35 @@ public class KtblDataHelper {
 	public static LinkedHashMap<String,String> getLivestockType() {
 		LinkedHashMap<String,String> livestock = new LinkedHashMap<String,String>();
 		
-		Map livestockProp = loadPropertiesFile("ktbl.livestock.properties", new Properties());
+		Map<String, String> livestockProp = (Map) loadPropertiesFile("ktbl.livestock.properties", new Properties());
 		
 			livestock.put(ZettelModel.ZETTEL_NULL, "Bitte wählen Sie...");
 			livestock.put("info.ktbl.livestock.cattle.de", "Rind");
-			livestock.put("info.ktbl.livestock.pork.de", "Schwein");
+			//livestock.put("info.ktbl.livestock.pork.de", "Schwein");
 			livestock.put("info.ktbl.livestock.chicken.de", "Huhn");
 			livestock.put("info.ktbl.livestock.turkey.de", "Pute");
 			livestock.put("info.ktbl.livestock.duck.de", "Ente");
 		
-		livestock.putAll(livestockProp);
+		livestockProp.putAll(livestock);
 		
 		return livestock;
 	}
 
 	public static Properties loadPropertiesFile(String fName, Properties properties) {
-		/*try (InputStream propStream = Thread.currentThread().getContextClassLoader()
+		try (InputStream propStream = Thread.currentThread().getContextClassLoader()
 				.getResourceAsStream("conf/" + fName)) {
 			properties.load(propStream);
 		} catch (IOException e) {
-			e.printStackTrace(); */
+			e.printStackTrace();
 			Properties defaultProperties = new Properties();
 			defaultProperties.put(ZettelModel.ZETTEL_NULL, "Bitte wählen Sie...");
 			defaultProperties.put("info.ktbl.livestock.cattle.de", "Rind");
 			defaultProperties.put("info.ktbl.livestock.pork.de", "Schwein");
+			defaultProperties.put("info.ktbl.livestock.pork.de", "Schaf");
 			
 			return defaultProperties;
-		//}
-		//return properties;
+		}
+		return properties;
 	}
 
 
