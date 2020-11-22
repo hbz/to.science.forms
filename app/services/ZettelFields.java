ea519ab8494d046e01740449fe8de2f1e6d10792
Andres Quast <quast@hbz-nrw.de>
2020-11-22 13:00:43 +0100

    fix labels for forms

--- app/services/ZettelFields.java
+++ app/services/ZettelFields.java
@@ -199,16 +199,14 @@ public class ZettelFields {
 			ZettelHelper.etikett.getEtikett("info:regal/zettel/ktblHeader");
 	public static Etikett livestockZF =
 			ZettelHelper.etikett.getEtikett("info:regal/zettel/livestock");
-	public static Etikett lShandlingZF =
-			ZettelHelper.etikett.getEtikett("info:regal/zettel/lShandling");
 	public static Etikett projecttitleZF =
-			ZettelHelper.etikett.getEtikett("info:regal/zettel/ktblProjectTitle");
+			ZettelHelper.etikett.getEtikett("info.ktbl.projecttitle@de");
 	public static Etikett treatmentZF =
-			ZettelHelper.etikett.getEtikett("info:regal/zettel/ktblTreatment");
+			ZettelHelper.etikett.getEtikett("info.ktbl.treatment@de");
 	public static Etikett housingZF =
-			ZettelHelper.etikett.getEtikett("info:regal/zettel/ktblHousing");
+			ZettelHelper.etikett.getEtikett("info.ktbl.housing@de");
 	public static Etikett treatmentdetailZF =
-			ZettelHelper.etikett.getEtikett("info:regal/zettel/ktblTreatmentDetail");
+			ZettelHelper.etikett.getEtikett("info.ktbl.treatmentdetail@de");
 
 	
 	private static HashMap<String, Etikett> etiketts = new HashMap();
