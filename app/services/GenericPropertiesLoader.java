8b475a9a4a8f824e177db2098dc72bdf036811bb
Andres Quast <quast@hbz-nrw.de>
2021-12-18 18:33:41 +0100

    make properties read into map as an determined sequence, not by random read

--- app/services/GenericPropertiesLoader.java
+++ app/services/GenericPropertiesLoader.java
@@ -24,8 +24,13 @@ public class GenericPropertiesLoader {
 	public Map<String, String> loadVocabularyMap(String propertiesFileName) {
 		Map<String, String> vocabMap = new LinkedHashMap<>();
 		String propertiesPath = play.Play.application().resource(propertiesFileName).getPath();
-		vocabMap = loadMapFromFile(propertiesPath);
-		return vocabMap;
+		try {
+			vocabMap = loadMapFromFile(propertiesPath);
+			return vocabMap;
+		} catch (Exception e) {
+			play.Logger.error(e.getMessage());
+		}
+		return null;
 	}
 
 	private Properties loadPropertiesFromFile(String propertiesPath) {
@@ -40,13 +45,17 @@ public class GenericPropertiesLoader {
 	};
 	private LinkedHashMap<String,String> loadMapFromFile(String propertiesPath) {
 		LinkedHashMap<String,String> map = new LinkedHashMap<>();
-		try (Reader propReader = new FileReader(propertiesPath)) {
+		try {
+			Reader propReader = new FileReader(propertiesPath);
 			BufferedReader br = new BufferedReader(propReader);
 			String line = null;
 			while((line=br.readLine())!=null) {
+				play.Logger.info("eingelesene Zeile: " + line);
 				String[] keyValue = line.split("=");
-				map.put(keyValue[0], keyValue[1]);	
-			}			
+				map.put(keyValue[0], keyValue[1]);
+				play.Logger.info(map.get(keyValue[0]));
+			}
+			br.close();
 			return map;
 		} catch (IOException e) {
 			e.printStackTrace();
