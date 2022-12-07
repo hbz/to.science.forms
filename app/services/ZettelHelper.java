5bc7ca5b3e07d5f52ebb461ba6845a7e51c58337
KayhanOgan <kayhan.ogan@hbz-nrw.de>
2022-12-07 09:27:12 +0100

    Fix citationReference

--- app/services/ZettelHelper.java
+++ app/services/ZettelHelper.java
@@ -359,7 +359,7 @@ public class ZettelHelper {
 		addError(result, errors, "other");
 		addError(result, errors, "containedIn");
 		addError(result, errors, "bibliographicCitation");
-		addErroe(result, errors, "citationReference");
+		addError(result, errors, "citationReference");
 		addError(result, errors, "issued");
 		addError(result, errors, "publicationYear");
 		addError(result, errors, "institution");
