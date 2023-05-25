f11c7b40b2cfd82027cc34d371e2359511de8e96
Dr. Ingolf Kuss <kuss@hbz-nrw.de>
2023-05-25 14:18:22 +0200

    Adapted bin/deploy.sh and bin/install.sh für neue Verzeichnisstruktur, Namenskonventionen

---
+++ bin/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export TERM=xterm-color
+deployingApp="to.science.forms"
+# branch=$(git status | grep 'On branch' | cut -d ' ' -f3)
+# echo "git's current branch is: "$branch
+# if [ ! -z "$1" ]; then
+#     branch="$1"
+# fi
+
+cd /opt/toscience/git/$deployingApp
+# git pull origin $branch
+/opt/toscience/activator/activator dist
---
+++ bin/install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if (( $EUID == 0 )); then
+    echo "Don't run as root!"
+    exit
+fi
+
+appDeployDir=$(pwd)
+toscienceDir="/opt/toscience"
+deployDir="/opt/toscience/git"
+targetDir="/opt/toscience/apps"
+linkDir="toscience-forms"
+fileName="zettel-1.0-SNAPSHOT.zip"
+folderName="zettel-1.0-SNAPSHOT"
+newInstallDir="$linkDir-$(date +'%Y%m%d%H%M%S')"
+confDir="/etc/toscience/forms"
+
+cp $appDeployDir/target/universal/$fileName $deployDir
+
+cd $deployDir
+unzip $fileName
+
+mv $deployDir/$folderName $targetDir/$newInstallDir
+mkdir $targetDir/$newInstallDir/logs
+
+if [ -L $toscienceDir/$linkDir ]; then
+    rm $toscienceDir/$linkDir
+fi
+ln -sf $targetDir/$newInstallDir $toscienceDir/$linkDir
+rm -r $targetDir/$newInstallDir/conf
+ln -sf $confDir $targetDir/$newInstallDir/conf
+cd $appDeployDir
+
+echo ""
+echo "Neue Binärversion verfügbar unter $targetDir/$newInstallDir."
+echo "Port ist fest eingestellt auf: 9003"
+echo "Zum Umschalten auf die neue Version:"
+echo "sudo service toscience-forms stop"
+echo "sudo service toscience-forms start"
+
